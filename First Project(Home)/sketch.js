4a7dada024d90a896151d4c865fe9c2c03aa5e5f
KyleD123 <Kyledoerksen75@yahoo.ca>
2020-02-05 10:35:49 -0800

    Moving box
    The moving box now changes colors with the arrow keys.

--- First Project(Home)/sketch.js
+++ First Project(Home)/sketch.js
@@ -10,22 +10,23 @@ let value = 0;
 function keyPressed(){
  if(keyCode === RIGHT_ARROW){
     x = x + 10;
-    
+    fill(200,0,0);
   }
   if(keyCode === LEFT_ARROW){
     x = x - 10;
-    
+    fill(0,200,0);
   } 
   if(keyCode === UP_ARROW){
     y = y - 10;
+    fill(0,0,200);
   }
   if(keyCode === DOWN_ARROW){
     y = y + 10;
+    fill(200,0,200);
   }
 }
 
 function man(){
-  fill(value);
   rect(x,y,50,50);
 }
 
@@ -37,6 +38,7 @@ function setup() {
 function draw() {
   background(255);
   man(x,y);
+  
   
 }
 

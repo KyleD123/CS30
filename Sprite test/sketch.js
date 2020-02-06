bd0c3857d652d1c0533d82c4a2e730af06f4bda6
KyleD123 <Kyledoerksen75@yahoo.ca>
2020-02-06 03:51:10 -0800

    got the sprite to appear
    doing aight

--- Sprite test/sketch.js
+++ Sprite test/sketch.js
@@ -2,43 +2,34 @@
 //Kyle Doerksen
 // A test of sprites
 //img = ['assets/Still.png','assets/Down1.png','assets/DOWN2.png'];
-let v;
-let img;
+let a,b,c;
 let x,y;
 
 function preload(){
-  loadImage('assets/Stll.png');
-  loadImage('assets/Down1.png');
-  loadImage('assets/DOWN2.png');
+  //a = loadImage('assets/Stll.png');
+  b = loadImage('assets/Down1.png');
+  c = loadImage('assets/DOWN2.png');
   
-}
-
-function animation(){
-  for(let img = 0; v = 2; v ++;){
-    if(v =< 0){
-      img = loadImage('assets/Still.png');
-      image(img,400,0);
-      v = v ++;
-    }
-    if(v > 0, v <2){
-      img = loadImage('assets/Down1.png');
-      image(img,400,0);
-      v = v ++;
-    }
-    if(v => 2){
-      img = loadImage('assets/DOWN2.png');
-      v = v - 2;
-      image(img,400,0);
-    }
-  }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
 }
 
 function draw() {
   background(220);
-  animation();
+  image(c,150,150);
+  let v = 1;
+  if(v === 1){
+    i = b;
+    image(v,400,0);
+    v = v ++;
+    }
+  if(v === 2){
+    i = c;
+    v = v - 1;
+    image(v,400,0);
+    }
   
 }

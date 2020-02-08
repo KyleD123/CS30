51ada5989e9131fa93cae24f4b5c8efb8bf89549
KyleD123 <Kyledoerksen75@yahoo.ca>
2020-02-07 23:50:27 -0800

    Background and borders
    I added a background and border for the site, as well as some shape changes when you click the mouse

--- First Project(Home)/sketch.js
+++ First Project(Home)/sketch.js
@@ -6,38 +6,58 @@
 let x = 555;
 let y = 500;
 let value = 0;
+let m = 0;
+
+
+
+function sky(){
+  fill(0,250,200);
+  rect(0,0,2000,1500);
+}
+function bord(){
+  fill(0);
+  rect(0,0,10,1000);
+  rect(0,0,2000,10)
+  rect(1270,0,10,1000);
+  rect(0,600,2000,10);
+}
+
+function count(){
+  if(keyIsPressed === true){
+    m = m +1;
+    print(m);
+  }
+}
 
 function keyPressed(){
  if(keyCode === RIGHT_ARROW){
     x = x + 10;
-    fill(200,0,0);1
+    
   }
   if(keyCode === LEFT_ARROW){
     x = x - 10;
-    fill(0,200,0);
+    
   } 
   if(keyCode === UP_ARROW){
     y = y - 10;
-    fill(0,0,200);
+    
   }
   if(keyCode === DOWN_ARROW){
     y = y + 10;
-    fill(200,0,200);
-  }
+    
+  } 
 }
-function bad(){
-  triangle(100,100,120,120,80,120)
-
-}
-
-function coord(){
-  if(mouseIsPressed){
-    print("That's not going to work");
-  }
-}
+ 
 
 function man(){
-  rect(x,y,50,50);
+  keyPressed();
+  fill(200);
+  strokeWeight(5);
+  rect(x,y,75,50);
+  if(mouseIsPressed === true){
+    rect(x,y,100,100);
+  }
+  
 }
 
 function setup() {
@@ -47,8 +67,10 @@ function setup() {
 
 function draw() {
   background(255);
+  sky();
+  bord();
   man();
-  bad();
+  count();
   
   
 }

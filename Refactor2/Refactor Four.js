332263aa837c6529d9950f5457d7df5972795a2a
KyleD123 <Kyledoerksen75@yahoo.ca>
2020-03-09 12:47:06 -0600

    refactor
    finished refactoring

--- Refactor2/Refactor Four.js
+++ Refactor2/Refactor Four.js
@@ -1,137 +1,32 @@
 //Chess Board
+
+const LENGTH = 50;
+
 function setup() {
-createCanvas(600, 600);
+  createCanvas(600, 600);
 }
-function draw() {
-fill(255);
-rect(0,0,75,75);
-fill(0);
-rect(0,75,75,75);
-fill(255);
-rect(0,150,75,75);
-fill(0);
-rect(0,225,75,75);
-fill(255);
-rect(0,300,75,75);
-fill(0);
-rect(0,375,75,75);
-fill(255);
-rect(0,450,75,75);
-fill(0);
-rect(0,525,75,75);
-fill(0);
-rect(75,0,75,75);
-fill(255);
-rect(75,75,75,75);
-fill(0);
-rect(75,150,75,75);
-fill(255);
-rect(75,225,75,75);
-fill(0);
-rect(75,300,75,75);
-fill(255);
-rect(75,375,75,75);
-fill(0);
-rect(75,450,75,75);
-fill(255);
-rect(75,525,75,75);
-fill(255);
-rect(150,0,75,75);
-fill(0);
-rect(150,75,75,75);
-fill(255);
 
-rect(150,150,75,75);
-fill(0);
-rect(150,225,75,75);
-fill(255);
-rect(150,300,75,75);
-fill(0);
-rect(150,375,75,75);
-fill(255);
-rect(150,450,75,75);
-fill(0);
-rect(150,525,75,75);
-fill(0);
-rect(225,0,75,75);
-fill(255);
-rect(225,75,75,75);
-fill(0);
-rect(225,150,75,75);
-fill(255);
-rect(225,225,75,75);
-fill(0);
-rect(225,300,75,75);
-fill(255);
-rect(225,375,75,75);
-fill(0);
-rect(225,450,75,75);
-fill(255);
-rect(225,525,75,75);
-fill(255);
-rect(300,0,75,75);
-fill(0);
-rect(300,75,75,75);
-fill(255);
-rect(300,150,75,75);
-fill(0);
-rect(300,225,75,75);
-fill(255);
-rect(300,300,75,75);
-fill(0);
-rect(300,375,75,75);
-fill(255);
-rect(300,450,75,75);
-fill(0);
-rect(300,525,75,75);
-fill(0);
+function chessBoard(){// Function that builds the chess board
+  for (let i = 0; i < 8; i++){
+    for(let q = 0; q < 8; q++){
+      if (q % 2 === 0 && i % 2 === 0){
+        fill(255);
+      }
+      else if (q % 2 === 0 && i % 2 !== 0){
+        fill(0);
+      }
+      else if (q % 2 !== 0 && i % 2 !== 0){
+        fill(255);
+      }
+      else if (q % 2 !== 0 && i % 2 === 0){
+        fill(0);
+      }
+      rect(LENGTH*q,LENGTH*i,LENGTH,LENGTH);
+    }
+  }
+  
+}
 
-rect(375,0,75,75);
-fill(255);
-rect(375,75,75,75);
-fill(0);
-rect(375,150,75,75);
-fill(255);
-rect(375,225,75,75);
-fill(0);
-rect(375,300,75,75);
-fill(255);
-rect(375,375,75,75);
-fill(0);
-rect(375,450,75,75);
-fill(255);
-rect(375,525,75,75);
-fill(255);
-rect(450,0,75,75);
-fill(0);
-rect(450,75,75,75);
-fill(255);
-rect(450,150,75,75);
-fill(0);
-rect(450,225,75,75);
-fill(255);
-rect(450,300,75,75);
-fill(0);
-rect(450,375,75,75);
-fill(255);
-rect(450,450,75,75);
-fill(0);
-rect(450,525,75,75);
-fill(0);
-rect(525,0,75,75);
-fill(255);
-rect(525,75,75,75);
-fill(0);
-rect(525,150,75,75);
-fill(255);
-rect(525,225,75,75);
-fill(0);
-rect(525,300,75,75);
-fill(255);
-rect(525,375,75,75);
-fill(0);
-
-rect(525,450,75,75);
-fill(255);
-rect(525,525,75,75);
+function draw() {// function that calls the chessboard function
+  chessBoard();
 }
--- Refactor2/sketch.js
+++ Refactor2/sketch.js
@@ -1,15 +1,14 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+//Black and White Target
+const SIZE = 400;
+const END = 40;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(240);
+  for (let i = SIZE; i >= END; i -= END) {// draws the bullseye
+    ellipse(width/2, height/2, i, i);
+  }
 }

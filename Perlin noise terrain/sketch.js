bb31727ff52cae09a4cfef0cda5add87e5b25165
KyleD123 <Kyledoerksen75@yahoo.ca>
2020-03-10 13:51:29 -0600

    Finished
    I have completed the perlin noise terrain assingment

--- Perlin noise terrain/sketch.js
+++ Perlin noise terrain/sketch.js
@@ -7,36 +7,46 @@ let newWidth = 10;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CORNERS);
-  
-  
+  fill(random(50,100),random(25,200),random(0,255));
 }
-
-function keyPressed(){
-  if ( keyCode === RIGHT_ARROW) {
+function checkKey() {
+  if (keyIsDown(RIGHT_ARROW)) {
     newWidth += 3;
   }
-  else if ( keyCode === LEFT_ARROW) {
+  else if (keyIsDown(LEFT_ARROW)) {
     newWidth -= 3;
-    if (newWidth <= 0)
-    newWidth += 1;
   }
 }
 
 
 function generateTerrain() {
-  
+  let tallest = height;
+  let tallestX = 0;
+
   for (let i = 0; i < width; i += newWidth) {
-    rectHeight = noise(rectTime);
+    let rectHeight = noise(rectTime);
     rectHeight = map(rectHeight, 0, 1, 0, height);
     rect(i, height, i + newWidth, rectHeight);
-    rectTime += 0.02;
-    
+    rectTime += 0.025;
+    if (rectHeight < tallest) {
+      tallest = rectHeight;
+      tallestX = i;
+    }
   }
+  line(tallestX,tallest,tallestX,tallest - 25);
+  triangle(tallestX ,tallest - 25,tallestX + 15, tallest - 15,tallestX, tallest - 7.5);
 }
+
 
 function draw() {
   rectTime = 10;
   background(220);
   generateTerrain();
-
+  print(newWidth);
+  if (frameCount % 2 === 0) {
+    checkKey();
+  }
+  if (newWidth <= 1) {
+    newWidth = 4;
+  }
 }

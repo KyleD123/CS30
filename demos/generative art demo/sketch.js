0bd07a120ace4951ba24787fe614e46c6b38995d
KyleD123 <Kyledoerksen75@yahoo.ca>
2020-03-11 14:10:08 -0600

    Started grid stuff
    cubic disarray

--- demos/generative art demo/sketch.js
+++ demos/generative art demo/sketch.js
@@ -1,13 +1,31 @@
-// 
+// Cubic disarray
+
+const PADDING = 60;
+let gridSpacing = 20;
+
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+  rectMode(CENTER);
 }
 
 function draw() {
   background(220);
-  
+  border();
+  drawGrid();
 }
 
+function drawGrid(){
+  for (let x = gridSpacing/2 + PADDING; x < width - PADDING; x += gridSpacing){
+    for (let y = gridSpacing/2 + PADDING; y < height - PADDING; y += gridSpacing){
+      rect(x,y,gridSpacing,gridSpacing);
+    }
+  }
+}
+
+function border() {
+  strokeWeight(15);
+  rect(width/2, height/2, width, height);
+  strokeWeight(2);
+}
